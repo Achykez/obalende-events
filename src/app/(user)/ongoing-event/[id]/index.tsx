d297be2a2b496487811ec080fca3f2a96c62e9e8
Achike <125678027+Achykez01@users.noreply.github.com>
2025-02-02 20:41:12 +0100

    added route check for ongoing

--- src/app/(user)/ongoing-event/[id]/index.tsx
+++ src/app/(user)/ongoing-event/[id]/index.tsx
@@ -1,10 +1,12 @@
-"use client"
+"use client";
 import React, { FC, useMemo } from "react";
 import OngoingHeader from "./ongoing-header";
 import { OngoingBody } from "./ongoing-body";
 import { useGetEventsQuery } from "@/redux/api/events";
-import {  useGetParticipantVotesQuery } from "@/redux/api/participants";
+import { useGetParticipantVotesQuery } from "@/redux/api/participants";
 import { Spin } from "antd";
+import { EventStatus } from "@/config";
+import { useRouter } from "next/navigation";
 
 interface IProps {
   id: string;
@@ -13,10 +15,14 @@ const OngoingEvents: FC<IProps> = ({ id }) => {
   const { data: eventsData, isLoading } = useGetEventsQuery({ id });
   const { data: participantsData, isLoading: isGettingParticipants } =
     useGetParticipantVotesQuery(id);
+  const router = useRouter();
 
   const eventDetails = useMemo(() => {
     return eventsData?.data?.find((event) => event.event._id === id) || null;
   }, [id, eventsData?.data]);
+  if (eventDetails && eventDetails.event.status !== EventStatus.ongoing) {
+    router.replace("/");
+  }
   return (
     <Spin spinning={isLoading || isGettingParticipants} size={"default"}>
       <OngoingHeader data={eventDetails} />
